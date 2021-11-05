5a59a0bc0b974d8437ef7d4a5f5623e64590d2a9
shruthishruu05 <shruthi.m@perfios.com>
2021-11-05 16:09:35 +0530

    ADDED : log message in main application

--- src/main/java/com/bridgelabz/addressbookservice/AddressBookServiceApplication.java
+++ src/main/java/com/bridgelabz/addressbookservice/AddressBookServiceApplication.java
@@ -3,11 +3,15 @@ package com.bridgelabz.addressbookservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class AddressBookServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookServiceApplication.class, args);
+		log.info("Addrress book app started");
 	}
 
 }
