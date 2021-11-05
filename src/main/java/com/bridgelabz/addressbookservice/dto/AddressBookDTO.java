2c7c083453a295f6bb786971339d79c3a5d0b082
shruthishruu05 <shruthi.m@perfios.com>
2021-11-05 12:59:10 +0530

    ADDED : required attributed for address book service

--- src/main/java/com/bridgelabz/addressbookservice/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookservice/dto/AddressBookDTO.java
@@ -1,64 +1,25 @@
 package com.bridgelabz.addressbookservice.dto;
 
 public class AddressBookDTO {
-	private int id;
-    private String name;
-    private String address;
-    private String phoneNo;
-    private String email;
-    
+	
+    public String name;
+    public String address;
+    public String phoneNo;
+    public String email;
+
+	public AddressBookDTO(String name, String address, String phoneNo, String email) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.phoneNo = phoneNo;
+		this.email = email;
+	}
+
 	@Override
 	public String toString() {
-		return "AddressBookDTO [id=" + id + ", name=" + name + ", address=" + address + ", phoneNo=" + phoneNo
-				+ ", email=" + email + "]";
+		return "AddressBookDTO [name=" + name + ", address=" + address + ", phoneNo=" + phoneNo + ", email=" + email
+				+ "]";
 	}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getPhoneNo() {
-		return phoneNo;
-	}
-
-	public void setPhoneNo(String phoneNo) {
-		this.phoneNo = phoneNo;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public AddressBookDTO(int id, String name, String address, String phoneNo, String email) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.address = address;
-		this.phoneNo = phoneNo;
-		this.email = email;
-	}
+	
 }
